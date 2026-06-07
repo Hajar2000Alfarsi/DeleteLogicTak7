1b6c9bcf2f55e9d3743a90445ea3f908b82cb772
Hajar <hajar2000alfarsi@gmail.com>
2026-06-07 14:45:27 +0400

    Create memeber list and initialize some data and adding new

--- src/LibraryManager.java
+++ src/LibraryManager.java
@@ -1,2 +1,20 @@
+import java.util.ArrayList;
+
 public class LibraryManager {
+    private ArrayList<LibraryMember> members;
+
+    public LibraryManager(){
+        members = new ArrayList<>();
+    }
+
+    public void initializeMembers() {
+        members.add(new LibraryMember("M101", "Ahmed", "Standard"));
+        members.add(new LibraryMember("M102", "Sara", "Premium"));
+        members.add(new LibraryMember("M103", "John", "Standard"));
+    }
+
+    public void addMember(LibraryMember member) {
+        members.add(member);
+    }
+
 }
