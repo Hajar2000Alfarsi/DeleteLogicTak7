52ed063e511cf3f771c126f2b1c3bc63263b9739
Hajar <hajar2000alfarsi@gmail.com>
2026-06-07 12:24:35 +0400

    Create DeleteTaskDemo class with arraylist with samples

---
+++ src/DeleteTaskDemo.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class DeleteTaskDemo {
+    public static void main(String[] args) {
+        ArrayList<String> tasks = new ArrayList<>();
+        tasks.add("Buy groceries");
+        tasks.add("Call doctor");
+        tasks.add("Finish report");
+    }
+}
